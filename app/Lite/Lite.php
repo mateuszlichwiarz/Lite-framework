89cef043fc9889e6fbdab097fc72bfc1581a3ac2
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-07-17 04:11:43 +0200

    throw all stuff to Lite class

---
+++ app/Lite/Lite.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Lite;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+
+class Lite
+{
+    protected $matcher;
+    protected $resolver;
+
+    public function __construct(UrlMatcher $matcher, ControllerResolver $resolver)
+    {
+        $this->matcher = $matcher;
+        $this->resolver = $resolver;
+    }
+
+    public function handle(Request $request)
+    {
+        $this->matcher->getContext()->fromRequest($request);
+
+        try {
+            $request->attributes->add($this->matcher->match($request->getPathInfo()));
+
+            $controller = $this->resolver->getController($request);
+
+            $arguments = $this->resolver->getArguments($request, $controller);
+
+            return call_user_func_array($controller, $arguments);
+        }catch(ResourceNotFoundException $e) {
+            return new Response('Not Found', 404);
+        }catch(\Exception $e) {
+            return new Response('Error: ' . $e->getMessage() . '<br> File: ' . $e->getFile() . '<br> Line: ' . $e->getLine());
+        }
+    }
+
+
+}
--- public/index.php
+++ public/index.php
@@ -3,7 +3,6 @@
 require_once '../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing;
 
 $request = Request::createFromGlobals();
