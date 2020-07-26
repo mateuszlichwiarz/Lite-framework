eb63d894edd430cade85b014cd6fcc247b22f558
Mateusz Lichwiarz <mateusz.lichwiarz@gmail.com>
2020-07-26 03:54:02 +0200

    simple model as fuck

--- app/Example/Controller/ExampleController.php
+++ app/Example/Controller/ExampleController.php
@@ -5,10 +5,22 @@ namespace Example\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+use Example\Model\Example;
+
 class ExampleController
 {
     public function indexAction(Request $request, $page)
     {
-        return new Response('Hello world! Index nr. '.$page);
+        $test0 = new Example;
+        $test0->setName('Test0');
+        $test0->setDesc('This_is_a_test_nr_0');
+
+        $name = $test0->getName();
+        $desc = $test0->getDesc();
+
+        return new Response('Hello world! Index nr. '.$page.
+            '<br>Test name: '.$name.
+            '<br>Test description: '.$desc
+        );
     }
 }
---
+++ app/Example/Model/Example.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Example\Model;
+
+class Example
+{
+    public $name;
+    public $desc;
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setDesc(string $desc): self
+    {
+        $this->desc = $desc;
+
+        return $this;
+    }
+
+    public function getDesc(): ?string
+    {
+        return $this->desc;
+    }
+}
